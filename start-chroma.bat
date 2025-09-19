94637c16dedc7344941ffb41cbb29b44eef69fcb
ConstantlyGrowup <1337909699@qq.com>
2025-09-19 16:18:16 +0800

    Delete start-chroma.bat

--- start-chroma.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Chroma DB...
-echo.
-echo If you don't have Chroma installed, please run:
-echo pip install chromadb
-echo.
-echo Starting Chroma on http://localhost:8000
-chroma run --host localhost --port 8000 --path ./chroma_data
-pause
